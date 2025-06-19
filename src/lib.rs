1b795da6fce5155bb609519e2d13c07512d25ae4
Peer Sommerlund <peer.sommerlund@gmail.com>
2025-06-19 10:56:21 +0200

    feat: Click outside menu closes it

--- src/lib.rs
+++ src/lib.rs
@@ -493,6 +493,11 @@ impl<T: Clone> MenuState<T> {
                         parent.children[child_inx].borrow_mut().is_highlight = true;
                     }
                     self.select();
+                    return true;
+                } else if self.is_active() {
+                    // Click outside active menu will close it
+                    self.reset();
+                    return true;
                 }
             }
             /* drag is kinda' complicated .. so not yet
@@ -502,7 +507,7 @@ impl<T: Clone> MenuState<T> {
             */
             _ => return false,
         }
-        return true;
+        return false;
     }
 }
 
